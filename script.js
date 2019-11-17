1475d37ce623a86cd9b0c090566d5c91fa4005f3
BJ Clark <bjclark13@outlook.com>
2019-11-17 14:54:07 -0500

    Add interest form, more style tweaks

--- script.js
+++ script.js
@@ -1,41 +1,45 @@
 {
     class LostRiverRidgeLabs {
-    constructor() {
-        this.addToggleListener();
-        this.handleStickyScroll();
-    }
+        constructor() {
+            this.addToggleListener();
+            this.handleStickyScroll();
+        }
 
-    handleStickyScroll = () => {
-        let $header = document.querySelector('header');
-        let $main = document.querySelector('main');
+        handleStickyScroll = () => {
+            let $header = document.querySelector('header');
+            let $main = document.querySelector('main');
 
-        window.addEventListener('scroll', () => {
-            if ( window.scrollY > ( window.outerHeight  ) ) {
-               $header.classList.add('sticky');
-               $main.classList.add('sticky');
+            console.log($header.classList.contains('always-sticky'), 'always sticky<<<<');
+            if (!$header.classList.contains('always-sticky')) {
+                window.addEventListener('scroll', () => {
+                    if (window.scrollY > (window.outerHeight)) {
+                        $header.classList.add('sticky');
+                        $main.classList.add('sticky');
 
-            } else {
-               $header.classList.remove('sticky');
-               $main.classList.remove('sticky');
+                    } else {
+                        $header.classList.remove('sticky');
+                        $main.classList.remove('sticky');
+                    }
+                })
             }
-        })
+        }
+
+        addToggleListener = () => {
+            document.querySelectorAll('.toggle').forEach(($element) => {
+                $element.addEventListener('click', this.handleToggle);
+            })
+        }
+
+        handleToggle = (event) => {
+            console.log(event);
+            event.preventDefault();
+
+            let selector = event.target.dataset.toggle;
+
+            document.querySelectorAll(selector).forEach(($element) => {
+                $element.classList.toggle('hidden');
+            })
+        }
     }
-
-    addToggleListener = () => {
-        document.querySelectorAll('.toggle').forEach( ($element) => {
-            $element.addEventListener('click', this.handleToggle);
-        })
-    }
-
-    handleToggle = (event) => {
-        event.preventDeafult();
-
-        let selector = event.target.dataset.toggle;
-
-        document.querySelectorAll(selector).forEach( ($element) => {
-           $element.classList.toggle('hidden');
-        })
-    }
-}
     new LostRiverRidgeLabs();
 }
